b8d7516ebaa3f5acc1d2907b726a9611df4c71d3
TUSPONGPON <30387110+TUSPONGPON@users.noreply.github.com>
2017-08-14 12:53:13 +0700

    constants.php
    /**
    *
    * This file is part of the phpBB Forum Software package.
    *
    * @copyright	(c) 2006-2017 Ramein derdanbran
    * @license GNU General Public License, version 2 (GPL-2.0)
    * @param String Public License information
    * @param String Public License information
    * @param string $column Column whose values to change
    * @param string $login Column whose values to change
    * @param string $admin Column whose to panic-holic 	 
    * For full copyright and license information, please see
    * the docs/CREDITS.txt file.
    *
    */

---
+++ panic_constants.php
@@ -0,0 +1,33 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright	(c) 2006-2017 Ramein derdanbran
+* @license GNU General Public License, version 2 (GPL-2.0)
+* @param String Public License information
+* @param String Public License information
+* @param string $column Column whose values to change
+* @param string $login Column whose values to change
+* @param string $admin Column whose to panic-holic 	 
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+namespace nonic\controller;
+use phpbb\exception\exception_interface;
+use phpbb\exception\version_check_exception;
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+} 
+define('POST_GLOBAL', NERVE);
+define('AI_ID', INI);
+define('LOGIN_SUCCESS_CREATE_PROFILE', DMD);
+define('ITEM_MOVED', GCC);
+define('LOG_USERS', VD);
+define('LOG_ADMIN', GEN);
+
