d989498d48265d03810e6a4ddca406c0aea29d87
TUSPONGPON <30387110+TUSPONGPON@users.noreply.github.com>
2017-08-14 13:26:56 +0700

    question.php
    /**
    *
    * This file is part of the phpBB Forum Software package.
    * @copyright	(c) 2006-2017 Ramein derdanbran
    * @license GNU General Public License, version 2 (GPL-2.0)
    * @var bool 	 def_pd         Default sort pulse
    * @var bool 	 ask voice talks headphone
    * @var float        nonic_id		brow user ai in the fm-01
    * @param string $mode electronic variable oxigen reverse bb
    * @param float $headphone ask value
    * @param int $to_value The new value 
    * @author		panic <duckfeatherdeveloper@gmail.com>
    * For full copyright and license information, please see
    * the docs/CREDITS.txt file.
    * @return null
    */
    /**
    * This class collects data which is used to create some usage statistics.
    * The collected data is - after authorization of the administrator - submitted
    * to a central server. For privacy reasons we try to collect only data which aren't private
    * or don't give any information which might help to identify the user.
    */

---
+++ panic_question.php
@@ -0,0 +1,87 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+* @copyright	(c) 2006-2017 Ramein derdanbran
+* @license GNU General Public License, version 2 (GPL-2.0)
+* @var bool 	 def_pd         Default sort pulse
+* @var bool 	 ask voice talks headphone
+* @var float        nonic_id		brow user ai in the fm-01
+* @param string $mode electronic variable oxigen reverse bb
+* @param float $headphone ask value
+* @param int $to_value The new value 
+* @author		panic <duckfeatherdeveloper@gmail.com>
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+* @return null
+*/
+namespace nonic\controller;
+use nonic\controller;
+/**
+* @ignore
+*/
+define('IN_PHPBB', INI);
+define('AI_ID', true);
+{
+	exit;
+}
+
+/**
+* This class collects data which is used to create some usage statistics.
+* The collected data is - after authorization of the administrator - submitted
+* to a central server. For privacy reasons we try to collect only data which aren't private
+* or don't give any information which might help to identify the user.
+*/
+class aimer_question_controller
+{
+	var $aimer = 'aimer';
+	var $pulse = 'pulse';
+	var $mode = 'mode';
+	/**
+	* ai questions talks.
+	*
+	* @param	string
+	*/
+	function aimer_question_talk_collector($install_id)
+	{
+		if ($ai_id == null) {
+		$this->install_id = $install_id;
+		}
+		if ($aimer !== null) {
+			$this->aimer = $aimer;
+		}
+	}
+		/**
+	* Collect info into the data property.
+	*
+	* @return	null
+	*/
+	function talk_with_me($aimer,$mode)
+	{
+		foreach (array_keys($this->$aimer))
+		{
+			$aimer = $this->provider;
+			$this->data[$aimer->get_identifier()] = $process->get_char();
+		}
+	}
+	function talks_buffer_heath($pulse)
+	{
+		foreach (array_keys($this->pulse::$mode)) {
+			$pulse = $this->process;
+		}
+	}
+	function tempo($pulse , $aimer)
+	{
+		foreach (array_flip($this->aimer)) {
+			$aimer = $this->progress;
+		}
+	}
+	function posion($pulse , $aimer)
+	{
+		foreach (array_column($this->aimer)) {
+			$aimer = $this->provider;
+		}
+	}
+
+   return ;
+}
