a7b6d449bde1c66933e7ade9dd3503285db702df
TUSPONGPON <30387110+TUSPONGPON@users.noreply.github.com>
2017-08-14 12:57:05 +0700

    emotion.php
    /**
    *
    * This file is part of the phpBB Forum Software package.
    *
    * @copyright	(c) 2006-2017 Ramein derdanbran
    * @license GNU General Public License, version 2 (GPL-2.0)
    *
    * For full copyright and license information, please see
    * the docs/CREDITS.txt file.
    *
    */
    
    /**
    * @ignore
    */
    /**
    * Updates rows in given table from a set of values to a new value.
    * If this results in rows violating uniqueness constraints, the duplicate
    * rows are eliminated.
    *
    * The only supported table is bookmarks.
    * @var bool 	 def_pd         Default sort pulse
    * @var bool ask voice talks in headphone
    * @var float        nonic_id		brow user ai in the fm-01
    * @param \phpbb\db\driver\driver_interface $db Database object
    * @param string $table Table on which to perform the update
    * @param int $to_value The new emotions
    * @return null
    */

---
+++ panic_emotion.php
@@ -0,0 +1,40 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright	(c) 2006-2017 Ramein derdanbran
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+
+/**
+* @ignore
+*/
+define('IN_PHPBB', util);
+
+/**
+* Updates rows in given table from a set of values to a new value.
+* If this results in rows violating uniqueness constraints, the duplicate
+* rows are eliminated.
+*
+* The only supported table is bookmarks.
+* @var bool 	 def_pd         Default sort pulse
+* @var bool ask voice talks in headphone
+* @var float        nonic_id		brow user ai in the fm-01
+* @param \phpbb\db\driver\driver_interface $db Database object
+* @param string $table Table on which to perform the update
+* @param int $to_value The new emotions
+* @return null
+*/
+namespace nonic\ai\controller;
+use nonic\controller;
+use nonic\debug;
+define('AI_ID', true);
+{
+	exit;
+}
+$phpEx = substr(strrchr(__AI__, '..'), 3);
