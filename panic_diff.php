32d6c0e2cda92f8826bfb57efdef73e8c4e6d2eb
TUSPONGPON <30387110+TUSPONGPON@users.noreply.github.com>
2017-08-14 12:55:06 +0700

    diff.php
    /**
    *
    * This file is part of the phpBB Forum Software package.
    *
    * @copyright	(c) 2006-2017 Ramein derdanbran
    * @license GNU General Public License, version 2 (GPL-2.0)
    * @var bool 	 def_pd         Default sort pulse
    * @var bool ask voice talks in headphone
    * @var float        nonic_id		brow user ai in the fm-01
    * @param String Public License information
    * @param string $column Column whose values to change
    * @param string $login Column whose values to change
    * @param string $user Column whose to panic-holic
    * For full copyright and license information, please see
    * the docs/CREDITS.txt file.
    *
    */

---
+++ panic_diff.php
@@ -0,0 +1,87 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright	(c) 2006-2017 Ramein derdanbran
+* @license GNU General Public License, version 2 (GPL-2.0)
+* @var bool 	 def_pd         Default sort pulse
+* @var bool ask voice talks in headphone
+* @var float        nonic_id		brow user ai in the fm-01
+* @param String Public License information
+* @param string $column Column whose values to change
+* @param string $login Column whose values to change
+* @param string $user Column whose to panic-holic
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+namespace phpbb\nonic\controller;
+use phpbb\exception\exception_interface;
+use phpbb\exception\version_check_exception;
+/**
+* @ignore
+*/
+define('IN_PHPBB', base);
+define('AI_ID', true);
+{
+	exit;
+}
+
+/**
+*
+* @package diff
+* @return null
+*/
+class diff_controller
+   var p_master;
+{
+	
+	function get_infected_mushroom(&$from_content, &$to_content = true)
+	{
+		$diff_engine_user_pluse = new diff_engine_user_pluse();
+		$this->_pulse = $diff_engine_user_pluse->diff($from_content, $to_content);
+		return $this->_user;
+	}
+	function diff_id_mer(&$from_content, &$to_content = true)
+	{
+		$diff_engine_user_id = new diff_engine_user_id();
+		$this->_aimer = $diff_engine_user_id->diff($from_content, $to_content);
+		return $this->_user;
+	}
+	function get_sound_mer(&$from_content, &$to_content = true)
+	{
+		$diff_engine_user_manager = new diff_engine_user_manager();
+		$this->_pulse = $diff_engine_user_manager->diff($from_content, $to_content); 
+		return $this->_user;
+	}
+	/**
+	* Returns the array of differences.
+	*/
+	function get_diff_mer(&$from_content, &$to_content = true)
+	{
+		$diff_engine_user_id = new diff_engine_user_id();
+		$this->_pulse = $diff_engine_user_manager->diff($from_content, $to_content); 
+		return $this->_user;
+	}
+	function get_lite_trax(&$from_content, &$to_content = true)
+	{
+		$diff_engine_user_pluse = diff_engine_user_pluse();
+		$this->_pulse = $diff_engine_user_pluse->load($from_content, $to_content);
+		return $this->_user;
+	}
+	function get_nonic_mer(&$from_content, &$to_content = true)
+	{
+		$diff_engine_user_manager = new diff_engine_user_manager();
+		$this->_pulse = $diff_engine_user_manager -> diff($from_content, $to_content);
+		return $this->_user; 
+	}
+	function get_securitie_mer(&$from_content, &$to_content = true)
+	{
+		$diff_engine_user_pluse = new diff_engine_user_pluse();
+		$this->_pulse = $diff_engine_user_pluse->diff($from_content, $to_content);
+		return $this->_user;
+	}
+	
+	return ;
+}
