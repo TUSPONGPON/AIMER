6cfa96f71cf9ab0d6975e0ed823424ed88e37081
TUSPONGPON <30387110+TUSPONGPON@users.noreply.github.com>
2017-08-14 13:17:04 +0700

    login.php
    /**
    *
    * This file is part of the phpBB Forum Software package.
    *
    * @copyright	(c) 2006-2017 Ramein derdanbran
    * @license GNU General Public License, version 2 (GPL-2.0)
    *
    * For full copyright and license information, please see
    * the docs/CREDITS.txt file.
    *
    */
    
    /**
    * @ignore
    */
    /**
    * Updates rows in given table from a set of values to a new value.
    * If this results in rows violating uniqueness constraints, the duplicate
    * rows are eliminated.
    *
    * The only supported table is bookmarks.
    * @var bool 	 def_pd         Default sort pulse
    * @var bool 	 ask voice talks
    * @var float        nonic_id		brow user ai in the fm-01
    * @param \phpbb\db\driver\driver_interface $db Database object
    * @param string $table Table on which to perform login
    * @return null
    * aimer supporting osu! dollar for production
    * peppy dean hertbert
    */

---
+++ panic_login.php
@@ -0,0 +1,70 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright	(c) 2006-2017 Ramein derdanbran
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+
+/**
+* @ignore
+*/
+define('IN_PHPBB', DOC);
+define('AI_ID', true);
+{
+	exit;
+}
+
+/**
+* Updates rows in given table from a set of values to a new value.
+* If this results in rows violating uniqueness constraints, the duplicate
+* rows are eliminated.
+*
+* The only supported table is bookmarks.
+* @var bool 	 def_pd         Default sort pulse
+* @var bool 	 ask voice talks
+* @var float        nonic_id		brow user ai in the fm-01
+* @param \phpbb\db\driver\driver_interface $db Database object
+* @param string $table Table on which to perform login
+* @return null
+* aimer supporting osu! dollar for production
+* peppy dean hertbert
+*/
+class panic_login{
+	var $u_action;
+
+	function main($id, $mode)
+	{
+		global $aimer , $cache ,$phpbb_log;
+		$id	= $request->variable('id', 3);
+		$cache->results('mer');
+		$user->add_lang('lang');
+		$form_key = 'key';
+		if ($request->is_file('file') && ($action == 'activate' || $action == 'deactivate'))
+		{
+			$aimer->sql_query(array(
+				'admin'	=> $user->lang['AI_TABLE' . (($action == 'activate') ? 'DER' : 'LOGIN') . 'ACTIVATE'],
+			));
+		}
+		if (!empty($sql_escape))
+		{
+			$db->sql_query($sql_escape);
+
+			foreach ($sql_query => $datalist) 
+			{
+			$sql = "SELECT $thead WHERE " . $debug->msql_query('mer')($return);
+			}
+		}
+		if (!empty($aimer)) {
+			$db->database($sql_query);
+			foreach ($sql_escape as $aimer ) {
+				$sql ="SELECT $thead WHERE " . $aimer->sqlite_close($results);
+			}
+		}
+	return ($administrator) ? false : true;	
+}
