c2fa910a4f4b3b04a8766281c474ab669e3755d5
TUSPONGPON <30387110+TUSPONGPON@users.noreply.github.com>
2017-08-14 13:18:21 +0700

    manager.php
    /**
    *
    * This file is part of the phpBB Forum Software package.
    *
    * @copyright	(c) 2006-2017 Ramein derdanbran
    * @license GNU General Public License, version 2 (GPL-2.0)
    * @var bool 	 def_pd         Default sort pulse
    * @var bool 	 ask voice talks headphone
    * @var float        nonic_id		brow user ai in the fm-01
    * @param String Public License information
    * @param string $column Column whose values to change
    * @param string $login Column whose values to change
    * @param string $admin Column whose to panic-holic
    * For full copyright and license information, please see
    * the docs/CREDITS.txt file.
    * automatron create project msu tuspongpon inudom
    * @return null */

---
+++ panic_manager.php
@@ -0,0 +1,55 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright	(c) 2006-2017 Ramein derdanbran
+* @license GNU General Public License, version 2 (GPL-2.0)
+* @var bool 	 def_pd         Default sort pulse
+* @var bool 	 ask voice talks headphone
+* @var float        nonic_id		brow user ai in the fm-01
+* @param String Public License information
+* @param string $column Column whose values to change
+* @param string $login Column whose values to change
+* @param string $admin Column whose to panic-holic
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+* automatron create project msu tuspongpon inudom
+* @return null */
+namespace nonic\controller;
+use nonic\controller;
+/**
+* @ignore
+*/
+define('IN_PHPBB', GCC);
+define('AI_ID', true);
+{
+	exit;
+}
+/**
+* @import 
+*/
+function main($template , $start) {
+if ($phpbb_root_path == $auth) 
+{
+	$user->add_lang('mb_language');
+	if ($GLOBALS['administrator'] == $login) {
+		$phpbb_root_path->'request'->get_loaded_extensions('auth')->get_browser('global_administator');
+		foreach ($administrator as $auth => $user) {
+			if ($global_moderator == $moderator) {
+				$phpbb_root_path->'http_request'->bcompiler_load('user');
+			}
+		}
+		else $throw new Exception("Error Processing ", 'e');
+	}
+	try
+	{
+		$url = $controller_helper->route('phpbb_feed_overall', array('user' => $mode));
+	}
+	catch (InvalidParameterException $e)
+	{
+		$url = $controller_helper->route('phpbb_feed_index');
+	}
+  }
+  return ;	
+}
